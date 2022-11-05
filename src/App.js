3b699095c5b7d0ebc63ae86a75e7fa5b7a8e7687
Mustapha Slama <mustaphaslama141@gmail.com>
2022-11-05 17:03:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const NotFound = () => (
+  <>
+    <div className="notFound">
+
+      <div className='stars'></div>
+      <div className='stars2'></div>
+      <div className='stars3'></div>
+      <div className='title'>
+        <span className="text404">
+          404
+        </span>
+        <br />
+        <span>
+          PAGE NOT FOUND
+        </span>
+      </div>
     </div>
-  );
-}
 
-export default App;
+  </>
+
+);
+
+export default NotFound;
